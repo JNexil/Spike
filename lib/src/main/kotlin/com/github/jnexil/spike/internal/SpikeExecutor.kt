8fba39f7cc8c6298590429836e87fb0415b65891
Jamefrus <jamefrus@ya.ru>
2016-11-15 19:05:50 +0200

    Injected logging

--- lib/src/main/kotlin/com/github/jnexil/spike/internal/SpikeExecutor.kt
+++ lib/src/main/kotlin/com/github/jnexil/spike/internal/SpikeExecutor.kt
@@ -6,10 +6,12 @@ import org.eclipse.aether.*
 import org.eclipse.aether.collection.*
 import org.eclipse.aether.repository.*
 import org.eclipse.aether.resolution.*
+import su.jfdev.anci.logging.*
 import java.io.*
 
 
 object SpikeExecutor {
+    private val logger = Logger[SpikeExecutor::class]
     private val cache = File("temporary_cache")
     private val system = MavenRepositorySystemUtils.newServiceLocator().run {
         getService(RepositorySystem::class.java)
@@ -19,9 +21,13 @@ object SpikeExecutor {
 
     fun execute(spike: Spike) {
         val session = createSession(system)
+        logger.trace { "create session" }
         val collectResponse = collectDependencies(session, spike)
+        logger.debug { "collected dependencies: [${collectResponse.root.artifacts.joinToString()}]" }
         val resolveResponse = resolveDependencies(session, collectResponse)
+        logger.debug { "resolved dependencies: [${resolveResponse.root.artifacts.joinToString()}]" }
         for (artifact in resolveResponse.root.artifacts) {
+            logger.info { "try save artifact: $artifact" }
             artifact.saveArtifact(spike)
         }
     }
@@ -29,8 +35,9 @@ object SpikeExecutor {
     private fun org.eclipse.aether.artifact.Artifact.saveArtifact(spike: Spike) {
         if (extension == "jar") {
             val newFile = File(spike.output, fullName)
+            logger.info { "save artifact from: $file to $newFile" }
             file.copyTo(newFile)
-        }
+        } else logger.info { "skip artifact $this" }
     }
 
     private fun createSession(system: RepositorySystem): RepositorySystemSession {
@@ -52,10 +59,13 @@ object SpikeExecutor {
 
     private fun CollectRequest.addArtifacts(artifacts: Sequence<Artifact>) {
         for (artifact in artifacts) {
+            logger.info { "add artifact: $artifact" }
             for (dependency in artifact.dependencies) {
+                logger.info { "add dependency: $dependency" }
                 addManagedDependency(dependency)
             }
         }
+        logger.info { "Was added ${managedDependencies.size} dependencies" }
     }
 
     private fun resolveDependencies(session: RepositorySystemSession, collectResponse: CollectResult): DependencyResult = system.resolveDependencies(session) {
