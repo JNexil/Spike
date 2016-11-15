2607051ec7e46c46513013cf40920132501240b4
Jamefrus <jamefrus@ya.ru>
2016-11-15 19:06:43 +0200

    Add MavenArtifact.toString()

--- lib/src/main/kotlin/com/github/jnexil/spike/MavenArtifact.kt
+++ lib/src/main/kotlin/com/github/jnexil/spike/MavenArtifact.kt
@@ -1,10 +1,11 @@
 package com.github.jnexil.spike
 
 import com.google.common.collect.*
+import org.eclipse.aether.artifact.*
 import org.eclipse.aether.graph.*
 
 class MavenArtifact(value: String): Artifact, Sequence<Dependency> {
-    private val dependency = Dependency(org.eclipse.aether.artifact.DefaultArtifact(value), "compile")
+    private val dependency = Dependency(DefaultArtifact(value), "compile")
     override val dependencies: Sequence<Dependency> get() = this
     override val type: String get() = TYPE
 
@@ -13,4 +14,6 @@ class MavenArtifact(value: String): Artifact, Sequence<Dependency> {
     }
 
     override fun iterator(): Iterator<Dependency> = Iterators.singletonIterator(dependency)
+
+    override fun toString(): String = dependency.toString()
 }
